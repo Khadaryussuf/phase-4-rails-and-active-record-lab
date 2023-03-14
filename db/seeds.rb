0f12b6900b86b22de7fe6945a4ed75e6413e0ff3
Khadaryussuf <salatkhadar@gmail.com>
2023-03-14 19:36:44 +0300

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Student.create!(first_name: "Mohamed", last_name: "Yussuf", grade: 90)
+Student.create([{ first_name: 'Mohamed', last_name: 'Khadar', grade: 80}])
